8c8e633bb5ce552355dd2ecb7a23dd966bf97774
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-17 10:16:42 +0200

    ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,29 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 10:03:49 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/17 10:05:32 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/17 10:16:12 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
+	size_t	j;
 
+	if (dstsize == 0)
+		return (0);
+	i = 0;
+	while (dst[i])
+		i++;
+	j = 0;
+	while (src[i] && i < dstsize - 1)
+	{
+		dst[i] = src[j];
+		j++;
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(dst) + ft_strlen(src));
 }
