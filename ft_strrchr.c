b9ff688976825b97d48af669287c15689774fe22
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-17 10:59:34 +0200

    ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 10:40:44 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/17 10:59:13 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int	i;
+
+	if ((char)c == '\0')
+		return ((char *)s + ft_strlen(s));
+	i = ft_strlen(s);
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
+}
