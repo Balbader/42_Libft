7dee4e8d3ed85fee9da3d3f48a79ebc8955e930b
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-17 11:41:25 +0200

    ft_memcmp.c

---
+++ ft_memcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 11:14:44 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/17 11:41:11 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+
+	i = 0;
+	while (str1[i] && str2[i] && str1[i] == str2[i] && i < n)
+		i++;
+	return (str1[i] - str2[i]);
+}
