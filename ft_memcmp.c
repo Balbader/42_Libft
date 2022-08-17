a60aece32287215acec51ba0036beb8cc92b9026
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-17 11:42:14 +0200

    ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 11:14:44 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/17 11:41:11 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/17 11:42:06 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
-
 	i = 0;
 	while (str1[i] && str2[i] && str1[i] == str2[i] && i < n)
 		i++;
