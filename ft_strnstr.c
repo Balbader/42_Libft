618946855c33da9abd98f318a08fba993d7f1548
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-17 16:58:43 +0200

    ft_strnstr.c

---
+++ ft_strnstr.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/17 11:41:48 by baalbade          #+#    #+#             */
+/*   Updated: 2022/08/17 16:46:17 by baalbade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	j = 0;
+	i = 0;
+	while (i < len && haystack[i] && needle[j])
+	{
+		j = 0;
+		while (i + j < len && haystack[i + j] == needle[j] && haystack[i + j])
+			j++;
+		if (needle[j])
+			i++;
+	}
+	if (needle[j])
+		return (NULL);
+	return ((char *)haystack + i);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char str[] = "Hello World how are you?";
+	char need[] = "W";
+
+	printf ("strnstr: %s\n", strnstr(str, need, 4));
+	printf ("ft_strnstr: %s\n", ft_strnstr(str, need, 4));
+	return (0);
+}
