003079e6abefa84c7c04d9c1b4ddbb3cf244179b
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-20 17:08:15 +0200

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 10:44:45 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/20 16:49:20 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/20 17:08:04 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 int	ft_check_char(char c, char const *set)
 {
@@ -31,14 +30,27 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*res;
 	int		i;
-	int		start;
-	int		stop;
+	int		j;
+	int		k;
 
-	
+	if (!s1 || !set)
+		return (NULL);
+	i = 0;
+	while (s1[i] && ft_check_char(s1[i], set))
+		i++;
+	j = strlen(s1) - 1;
+	while (j > i && ft_check_char(s1[j], set))
+		j--;
+	res = (char *)malloc(sizeof(char) * (j - i + 2));
+	if (!res)
+		return (NULL);
+	k = 0;
+	while (i <= j)
+	{
+		res[k] = s1[i];
+		i++;
+		k++;
+	}
+	res[k] = '\0';
 	return (res);
-}
-
-int	main(void)
-{
-	return (0);
 }
