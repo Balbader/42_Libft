ce04e21343bb472cd1ecad963b0b7f9c92c3260d
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-20 11:59:58 +0200

    ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 10:44:45 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/20 11:26:20 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/20 11:54:30 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,14 @@ char	*ft_strtrim(char const *s1, char const *set)
 {
 	char			*res;
 	unsigned int	i;
+	unsigned int	j;
 
 	return (res);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	return (0);
 }
