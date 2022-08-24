b2cbbad0c6f841d0531175b58adb6d54545c3b94
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-24 10:48:41 +0200

    ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -1,142 +1,82 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_split.c                                         :+:      :+:    :+:   */
+/*   my_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/20 17:09:28 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/24 09:05:24 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/24 10:46:48 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_count_words(char const *s, char c)
+static char	*ft_strdup_spe(char const *s, char c)
 {
-	int	i;
-	int	count;
+	int		i;
+	char	*dst;
+
+	i = 0;
+	while (s[i] && s[i] != c)
+		i++;
+	dst = malloc(sizeof(char) * (i + 1));
+	if (!dst)
+		return (0);
+	i = 0;
+	while (s[i] && s[i] != c)
+	{
+		dst[i] = s[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (dst);
+}
+
+static size_t	ft_count_words(char const *s, char c)
+{
+	size_t	count;
+	size_t	i;
 
 	count = 0;
 	i = 0;
-	while (s[i] && s[i] == c)
-		i++;
 	while (s[i])
 	{
-		if (s[i] == c || s[i] == '\0')
+		while (s[i] == c)
+			i++;
+		if (s[i])
 		{
 			count++;
-			while (s[i] != '\0' && s[i + 1] == c)
+			while (s[i] && s[i] != c)
 				i++;
 		}
-		i++;
 	}
 	return (count);
 }
 
-int	*ft_strs_len(char const *s, char c)
-{
-	int	*strs_len;
-	int	str_len;
-	int	i;
-	int	j;
-
-	strs_len = (int *)malloc(sizeof(int) * (ft_count_words(s, c) + 1));
-	str_len = 0;
-	i = 0;
-	j = -1;
-	while (s[i] && s[i] == c)
-		i++;
-	while (s[i])
-	{
-		if (s[i] == c || s[i] == '\0')
-		{
-			while (s[i] != '\0' && s[i + 1] == c)
-				i++;
-			strs_len[++j] = str_len;
-			str_len = 0;
-			i++;
-		}
-		str_len++;
-		i++;
-	}
-	return (strs_len);
-}
-
-char	**ft_alloc_mem(char const *s, char c)
-{
-	char	**tab;
-	int		count;
-	int		*strs_len;
-	int		i;
-
-	count = ft_count_words(s, c);
-	tab = (char **)malloc(sizeof(char *) * (count + 1));
-	strs_len = ft_strs_len(s, c);
-	if (!tab)
-		return (NULL);
-	i = 0;
-	while (i < count)
-	{
-		tab[i] = (char *)malloc(sizeof(char) * (strs_len[i] + 1));
-		i++;
-	}
-	return (tab);
-}
-
-
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
-	int		count;
-	int		i;
-	int		j;
-	int		k;
+	size_t	words_nb;
+	size_t	i;
+	size_t	j;
 
-	if (!s || !c)
-		return (NULL);
-	count = ft_count_words(s, c);
-	tab = ft_alloc_mem(s, c);
-	k = 0;
+	if (!s)
+		return (0);
+	words_nb = ft_count_words(s, c);
+	tab = malloc(sizeof(char *) * (words_nb + 1));
+	if (!tab)
+		return (0);
 	i = 0;
-	while (s[i] && s[i] == c)
-		i++;
-	while (k < count)
+	j = 0;
+	while (j < words_nb)
 	{
-		j = 0;
-		while (s[i])
-		{
-			if (s[i] == c || s[i] == '\0')
-			{
-				while (s[i] != '\0' && s[i + 1] == c)
-					i++;
-				j++;
-				i++;
-			}
+		while (s[i] && s[i] == c)
 			i++;
-		}
-		k++;
+		tab[j++] = ft_strdup_spe(s + i, c);
+		while (s[i] && s[i] != c)
+			i++;
 	}
-	tab[k] = NULL;
+	tab[j] = 0;
 	return (tab);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char	str[] = "       Hello world, how       are you today?  	";
-	int		tot_count;
-	int		*lens;
-	int		i;
-
-	tot_count = ft_count_words(str, ' ');
-	lens = ft_strs_len(str, ' ');
-	i = 0;
-	while (i < tot_count)
-	{
-		printf("%d\n", lens[i]);
-		i++;
-	}
-	return (0);
 }
