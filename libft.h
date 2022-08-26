f4fbce70ffba10c3776db764737219bed66ef7ae
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-26 12:32:37 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 10:46:03 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/26 12:18:29 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/26 12:32:23 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,5 +58,6 @@ void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
 void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
