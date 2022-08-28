a456ebc36f6d187cd20eeda7ba69c2a83bec2de2
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-28 11:02:42 +0200

    ft_lstclear.c

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/28 10:49:59 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/28 10:53:58 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/28 11:02:26 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_lstclear(t_list **lst, void (*del)(void*))
 {
 	t_list	*ptr;
-	t_list *next;
+	t_list	*next;
 
 	ptr = *lst;
 	*lst = NULL;
