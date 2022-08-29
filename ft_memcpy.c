265ba60d5cd094075c1d6cad47a7f249159ca092
Balbader <15971200+Balbader@users.noreply.github.com>
2022-08-29 17:09:18 +0200

    ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 12:36:26 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/16 16:37:34 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/08/29 17:09:10 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	size_t			i;
 	unsigned char	*str;
 
-	if (!((unsigned char *)dst) || !str)
+	if (!((unsigned char *)dst) || !src)
 		return (0);
 	str = (unsigned char *)src;
 	i = 0;
