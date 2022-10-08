0825f7250fe8cb83776c8999da41c91d7973d8a3
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-08 22:42:39 +0200

    ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: baalbade <baalbade@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 19:05:37 by baalbade          #+#    #+#             */
-/*   Updated: 2022/08/17 19:25:47 by baalbade         ###   ########.fr       */
+/*   Updated: 2022/10/08 22:42:16 by baalbade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,15 @@ char	*ft_strdup(const char *s1)
 {
 	char	*cpy;
 	int		i;
+	int		j;
 
 	cpy = (char *)malloc(sizeof(char) * (ft_strlen(s1) + 1));
 	if (!cpy)
 		return (NULL);
+	j = 0;
 	i = 0;
 	while (s1[i])
-	{
-		cpy[i] = s1[i];
-		i++;
-	}
-	cpy[i] = '\0';
+		cpy[j++] = s1[i++];
+	cpy[j] = '\0';
 	return (cpy);
 }
