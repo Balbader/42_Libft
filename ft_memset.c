ba7a6983ab392593e0a03f73a107873618b475aa
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-22 20:08:19 +0200

    ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -15,13 +15,11 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t			i;
-	unsigned char	*temp;
 
 	i = 0;
-	temp = (unsigned char *)b;
 	while (i < len)
 	{
-		temp[i] = (unsigned char)c;
+		((unsigned char *)b)[i] = (unsigned char)c;
 		i++;
 	}
 	return (b);
