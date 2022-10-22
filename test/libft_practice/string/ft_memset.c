616073a0be602e6d0f2199cbfd1b85522fdbdafc
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-22 20:08:31 +0200

    ft_memset.c

--- test/libft_practice/string/ft_memset.c
+++ test/libft_practice/string/ft_memset.c
@@ -1,17 +1,26 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	unsigned char	*str;
 	size_t			i;
 
-	str = (unsigned char *)b;
 	i = 0;
 	while (i < len)
 	{
-		str[i] = (unsigned char)c;
+		((unsigned char *)b)[i] = (unsigned char)c;
 		i++;
 	}
-	str[i] = '\0';
 	return (b);
+}
+
+int	main(void)
+{
+	char	dst[] = "Hello World How Are You?";
+
+	ft_memset(dst, 'r', 10);
+	printf("%s\n", dst);
+	printf("%ld\n", strlen(dst));
+	return (0);
 }
