78321571b991d0cb6fd5c64922408f44f311d24b
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-23 12:22:38 +0200

    ft_strdup.c

--- test/libft_practice/string/ft_strdup.c
+++ test/libft_practice/string/ft_strdup.c
@@ -1,6 +1,20 @@
 #include <stdlib.h>
+#include <string.h>
 
 char	*ft_strdup(const char *s1)
 {
 	char	*cpy;
+	int		i;
+
+	cpy = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
 }
