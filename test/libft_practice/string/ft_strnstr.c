7e459d1d93b566ebb1bc094bd5a05575e641ddb5
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-27 16:26:42 +0200

    ft_strnstr.c

--- test/libft_practice/string/ft_strnstr.c
+++ test/libft_practice/string/ft_strnstr.c
@@ -1,22 +1,37 @@
 #include <stddef.h>
+#include <stdio.h>
+#include <string.h>
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
-	if (!needle)
-		return ((char *)haystack);
-	i = 0;
 	j = 0;
+	i = 0;
+	if (((char *)needle)[j] == '\0')
+		return ((char *)haystack);
 	while (((char *)haystack)[i])
 	{
-		while (((char *)haystack)[i] == ((char *)needle)[j])
+		while (((char *)haystack)[i] == ((char *)needle)[j] && i < len)
 		{
 			i++;
 			j++;
+			if (((char *)haystack)[i] != ((char *)needle)[j])
+			return ((char *)haystack + i);
 		}
 		i++;
 	}
 	return (NULL);
+}
+
+int	main(void)
+{
+	const char *haystack = "Hello how are you?";
+	const char *needle = "how";
+	// const char *needle = "";
+
+	printf("%s\n", ft_strnstr(haystack, needle, 10));
+	printf("%s\n", strnstr(haystack, needle, 10));
+	return (0);
 }
