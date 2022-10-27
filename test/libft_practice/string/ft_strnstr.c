a266f386586a2ed7a155e47b0b108e93678ac827
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-27 15:52:49 +0200

    ft_strnstr.c

--- test/libft_practice/string/ft_strnstr.c
+++ test/libft_practice/string/ft_strnstr.c
@@ -2,5 +2,21 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	i;
+	size_t	j;
+
+	if (!needle)
+		return ((char *)haystack);
+	i = 0;
+	j = 0;
+	while (((char *)haystack)[i])
+	{
+		while (((char *)haystack)[i] == ((char *)needle)[j])
+		{
+			i++;
+			j++;
+		}
+		i++;
+	}
 	return (NULL);
 }
