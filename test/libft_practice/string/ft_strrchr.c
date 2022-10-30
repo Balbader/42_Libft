b40de2fe8de5a141998563eef2c721dc68d9ce74
Balbader <15971200+Balbader@users.noreply.github.com>
2022-10-30 10:10:36 +0100

    ft_strrchr.c

--- test/libft_practice/string/ft_strrchr.c
+++ test/libft_practice/string/ft_strrchr.c
@@ -1,8 +1,32 @@
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+
 char	*ft_strrchr(const char *s, int c)
 {
-	int	i;
+	int				i;
+	unsigned char	needle;
+	char			*str;
 
-	i = 0;
-	while (s[i])
-	
+	if (!s)
+		return (NULL);
+	str = (char *)s;
+	needle = c;
+	i = strlen(str);
+	while (i > 0)
+	{
+		if (str[i] == needle)
+			return (str + i);
+		i--;
+	}
+	return (NULL);
+}
+
+int	main(void)
+{
+	char 	*str = "Hello world uhuu, uhuu, hola como estas uhuu, me too !!!";
+	int		needle = 'u';
+
+	printf("%s\n", ft_strrchr(str, needle));
+	return (0);
 }
