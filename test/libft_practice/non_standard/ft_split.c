bb9b103a8f41ad9b8fe709f61c0c5811aa68de66
Balbader <15971200+Balbader@users.noreply.github.com>
2022-11-04 20:05:36 +0100

    ft_split.c

---
+++ test/libft_practice/non_standard/ft_split.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char	**res;
+	return (res);
+}
